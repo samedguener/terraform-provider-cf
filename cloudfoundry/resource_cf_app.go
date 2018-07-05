f7e0328df705fbbfc92c09b93092fb932630c06a
Jim Carrothers <jim.carrothers@sap.com>
2018-07-05 14:15:13 -0700

    ensure cf_app.url field is not the empty string
    This is important because, with the archive changes, an empty string
    value causes the provider to wait forever.

--- cloudfoundry/resource_cf_app.go
+++ cloudfoundry/resource_cf_app.go
@@ -14,6 +14,7 @@ import (
 
 	// "github.com/hashicorp/terraform/helper/hashcode"
 	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/hashicorp/terraform/helper/validation"
 	"github.com/terraform-providers/terraform-provider-cf/cloudfoundry/cfapi"
 	"github.com/terraform-providers/terraform-provider-cf/cloudfoundry/repo"
 )
@@ -101,6 +102,7 @@ func resourceApp() *schema.Resource {
 				Type:          schema.TypeString,
 				Optional:      true,
 				ConflictsWith: []string{"git", "github_release"},
+				ValidateFunc:  validation.NoZeroValues,
 			},
 			"git": &schema.Schema{
 				Type:          schema.TypeList,
