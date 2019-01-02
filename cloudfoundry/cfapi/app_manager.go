d999f90823a73a5e395126ed5541ac74b30510b2
lixilin2301 <lixilin2301@gmail.com>
2019-01-02 11:08:40 +0100

    change stack to nil if docker image is offered and add a test

--- cloudfoundry/cfapi/app_manager.go
+++ cloudfoundry/cfapi/app_manager.go
@@ -297,6 +297,9 @@ func (am *AppManager) StartApp(appID string, timeout time.Duration) (err error) 
 		app.State = &AppStarted
 		// Maps are not set nit, set nil manually
 		app.DockerCredentials = nil
+		if app.DockerImage != nil && *app.DockerImage != ""{
+			app.StackGUID = nil
+		}
 		if app, err = am.UpdateApp(app); err != nil {
 			return err
 		}
